3d885bbd7bb4623e199210f226c4683c0569df79
Michael Nilsson <michaelnilsson90@hotmail.com>
2016-04-19 09:57:44 -0400

    reordered routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var index = require('./index.js');
 
 var app = express();
@@ -12,77 +13,82 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+
+// ID API
+// ======
+app.get('/:id(\\d+)/', function (req, res) {
+	console.log(1);
+	res.redirect(index.data[req.params.id]);
+});
+
+app.get('/image/:id/:date', function (req, res) {
+	console.log(2);
+	res.redirect(index.image[req.params.id] + req.params.date);
+});
+
+app.get('/data/:id/:date', function (req, res) {
+	console.log(3);
+	res.redirect(index.data[req.params.id] + req.params.date);
+});
 
 
 // Images
 // ======
 app.get('/gm/images/x0/:date', getfile, function (req, res) {
-	console.log(req.file)
+	console.log(4);
 	res.redirect(req.file);
 });
 
 app.get('/gm/images/y0/:date', getfile, function (req, res) {
-
+	console.log(5);
 	res.redirect(req.file);
 });
 
 app.get('/gm/images/z0/:date', getfile, function (req, res) {
-
+	console.log(6);
 	res.redirect(req.file);
 });
 
 // Metadata
 // ========
 app.get('/gm/data/x0/', function (req, res) {
-
-	res.redirect("../../meta/x0/info.txt");
+	console.log(7);
+	res.redirect("../../meta/x0/info.json");
 });
 
 app.get('/gm/data/y0/', function (req, res) {
-
-	res.redirect("../../meta/y0/info.txt");
+	console.log(8);
+	res.redirect("../../meta/y0/info.json");
 });
 
 app.get('/gm/data/z0/', function (req, res) {
-
-	res.redirect("../../meta/z0/info.txt");
+	console.log(9);
+	res.redirect("../../meta/z0/info.json");
 });
 
 // data
 // ====
 app.get('/gm/data/x0/:date', getfile, function (req, res) {
-	console.log(req.file)
+	console.log(10);
 	res.redirect(req.file);
 });
 
 app.get('/gm/data/y0/:date', getfile, function (req, res) {
-
+	console.log(11);
 	res.redirect(req.file);
 });
 
 app.get('/gm/data/z0/:date', getfile, function (req, res) {
-
+	console.log(12);
 	res.redirect(req.file);
 });
 
-// ID API
-// ======
-app.get('/:id', function (req, res) {
-	res.redirect(index.data[req.params.id]);
-});
-
-app.get('/image/:id/:date', function (req, res) {
-
-	res.redirect(index.image[req.params.id] + req.params.date);
-});
-
-app.get('/data/:id/:date', function (req, res) {
-	res.redirect(index.data[req.params.id] + req.params.date);
-});
 
 app.listen(3000, function () {
  	console.log('iswa test server is listening on port 3000');
 });
+
 
 
 
@@ -107,7 +113,6 @@ function getfile(req, res, next) {
 		}
 	});
 
-	console.log( fileIndex);
 	if(fileIndex == -1){
 		fileIndex = files.length - 1; 
 		req.file = files[fileIndex];
